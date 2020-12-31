4b21a88dfde04023f778d2f13b2b2d1a9c96ba48
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2020-12-31 03:46:16 +0100

    Adds function "copy" and extends "intersect" to accept more than 2 sets

--- Set.lua
+++ Set.lua
@@ -6,6 +6,14 @@ local function create(list)
     return set
 end
 
+local function copy(set)
+    local setCopy = {}
+    for key, value in pairs(set) do
+        setCopy[key] = value
+    end
+    return setCopy
+end
+
 local function size(set)
     local keys = Object.keys(set)
     return #keys
@@ -15,18 +23,30 @@ local function contains(set, value)
     return set[value] == true
 end
 
-local function intersect(setA, setB)
-    local set = {}
-    for value, _ in pairs(setA) do
-        if Set.contains(setB, value) then
-            set[value] = true
+local function intersect(...)
+    local sets = {...}
+    local set
+
+    if #sets >= 1 then
+        set = copy(sets[1])
+        for index = 2, #sets do
+            local setB = sets[index]
+            for key, _ in pairs(set) do
+                if set[key] and not setB[key] then
+                    set[key] = nil
+                end
+            end
         end
+    else
+        set = {}
     end
+
     return set
 end
 
 Set = {
     create = create,
+    copy = copy,
     size = size,
     contains = contains,
     intersect = intersect
