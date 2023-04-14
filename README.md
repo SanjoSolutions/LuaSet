362943fffe37451c7d1c2c0d0797b570e8d4ea3b
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-04-14 12:51:04 +0200

    Support for embedding

--- README.md
+++ README.md
@@ -42,3 +42,43 @@ This method also works for tables which fit the structure of set tables (just ca
 * **union**: creates a union of a set.
 * **hasElements**: checks if a set has any elements.
 * **isEmpty**: checks if a set is empty.
+
+## Usage
+
+### Embedding
+
+#### Introduction
+
+With this approach, the library is included as part of the add-on. A benefit to the add-on user is, that only
+the add-on appears in the add-on list. So it can be easier to enable or disable an add-on and its library dependencies
+compared to an approach where the libraries are included as add-ons (in this case each library shows as an add-on in the add-ons list and the user potentially is required to tick multiple checkboxes to enable or disable an add-on and its library dependencies).
+
+#### How to do it
+
+This library registers itself via the library "[Library](https://github.com/SanjoSolutions/LuaLibrary)". With this approach it is avoided to create another global (with its theoretical potential that other add-ons already use the same global). And "Library" also handles when multiple add-ons load the same library, potentially different versions of it.
+To embed it, also include "[Library](https://github.com/SanjoSolutions/LuaLibrary)" as a dependency to your add-on.
+
+In our example, we have installed both "Library", "Set" and the dependencies of "Set" ("Boolean" and "Object") inside the "libs" folder inside the add-on folder.
+
+In your add-ons TOC file add (to load "Library" and "Set" and its dependencies):
+
+```
+libs/Library/Library.xml
+libs/Boolean/Boolean.xml
+libs/Object/Object.xml
+libs/Set/Set.xml
+```
+
+It's recommended to load the library files first, so that the libraries are available in the other Lua files.
+
+In your Lua file(s), you can access the library via:
+
+```lua
+local Set = Library.retrieve('Set', '^1.0.0')
+```
+
+### As add-on
+
+This library can also just be put directly in the AddOns folder. In this case the library is available via the global `Set`.
+
+If you develop an add-on and do this approach, you can just include this library in your release as a folder next to your add-ons folder.
--- Set.lua
+++ Set.lua
@@ -1,107 +1,119 @@
-Set = {}
+local addOnName = 'Set'
+local version = '1.0.0'
 
-function Set:new(list)
-  return Set.create(list)
-end
+if (_G.Library and not Library.isRegistered(addOnName, version)) or not _G.Library then
+  --- @class Set
+  local Set = {}
 
-function Set.create(list)
-  local set = {}
-  setmetatable(set, {__index = Set})
-  if list then
-    for _, item in ipairs(list) do
-      Set.add(set, item)
-    end
+  function Set:new(list)
+    return Set.create(list)
   end
-  return set
-end
 
-function Set:add(element)
-  self[element] = true
-end
-
-function Set:copy()
-  local setCopy = {}
-  for key, value in pairs(self) do
-    setCopy[key] = value
-  end
-  return setCopy
-end
-
-function Set:iterator()
-  return pairs(self)
-end
-
-function Set:toList()
-  local list = {}
-  for value, _ in pairs(self) do
-    table.insert(list, value)
-  end
-  return list
-end
-
-function Set:size()
-  local keys = Object2.keys(self)
-  return #keys
-end
-
-function Set:contains(value)
-  return self[value] == true
-end
-
-function Set:containsWhichFulfillsCondition(condition)
-  for element in Set.iterator(self) do
-    if condition(element) then
-      return true
-    end
-  end
-  return false
-end
-
-function Set:remove(element)
-  self[element] = nil
-end
-
-function Set:intersect(...)
-  local sets = { self, ... }
-  local set
-
-  if #sets >= 1 then
-    set = Set.copy(sets[1])
-    for index = 2, #sets do
-      local setB = sets[index]
-      for key, _ in pairs(set) do
-        if set[key] and not setB[key] then
-          Set.remove(set, key)
-        end
+  function Set.create(list)
+    local set = {}
+    setmetatable(set, { __index = Set })
+    if list then
+      for _, item in ipairs(list) do
+        Set.add(set, item)
       end
     end
-  else
-    set = {}
+    return set
   end
 
-  return set
-end
+  function Set:add(element)
+    self[element] = true
+  end
 
-function Set:equals(setB)
-  local setASize = Set.size(self)
-  local setBSize = Set.size(setB)
-  return setASize == setBSize and Set.size(Set.intersect(self, setB)) == setASize
-end
-
-function Set:union(...)
-  local set = Set.create()
-  for __, set2 in ipairs({self, ...}) do
-    for element in Set.iterator(set2) do
-      set:add(element)
+  function Set:copy()
+    local setCopy = {}
+    for key, value in pairs(self) do
+      setCopy[key] = value
     end
+    return setCopy
   end
-  return set
-end
 
-function Set:hasElements()
-  return Boolean.toBoolean(next(self))
-end
+  function Set:iterator()
+    return pairs(self)
+  end
 
-function Set:isEmpty()
-  return not Set.hasElements(self)
+  function Set:toList()
+    local list = {}
+    for value, _ in pairs(self) do
+      table.insert(list, value)
+    end
+    return list
+  end
+
+  function Set:size()
+    local keys = Object2.keys(self)
+    return #keys
+  end
+
+  function Set:contains(value)
+    return self[value] == true
+  end
+
+  function Set:containsWhichFulfillsCondition(condition)
+    for element in Set.iterator(self) do
+      if condition(element) then
+        return true
+      end
+    end
+    return false
+  end
+
+  function Set:remove(element)
+    self[element] = nil
+  end
+
+  function Set:intersect(...)
+    local sets = { self, ... }
+    local set
+
+    if #sets >= 1 then
+      set = Set.copy(sets[1])
+      for index = 2, #sets do
+        local setB = sets[index]
+        for key, _ in pairs(set) do
+          if set[key] and not setB[key] then
+            Set.remove(set, key)
+          end
+        end
+      end
+    else
+      set = {}
+    end
+
+    return set
+  end
+
+  function Set:equals(setB)
+    local setASize = Set.size(self)
+    local setBSize = Set.size(setB)
+    return setASize == setBSize and Set.size(Set.intersect(self, setB)) == setASize
+  end
+
+  function Set:union(...)
+    local set = Set.create()
+    for __, set2 in ipairs({ self, ... }) do
+      for element in Set.iterator(set2) do
+        set:add(element)
+      end
+    end
+    return set
+  end
+
+  function Set:hasElements()
+    return Boolean.toBoolean(next(self))
+  end
+
+  function Set:isEmpty()
+    return not Set.hasElements(self)
+  end
+
+  if _G.Library then
+    Library.register(addOnName, version, Set)
+  else
+    _G.Set = Set
+  end
 end
